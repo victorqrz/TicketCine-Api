197fc99a00d1ebcb7869bad387bbe33b3d1913af
Victor Queiroz <victoorqrz@gmail.com>
2024-10-21 22:58:09 -0300

    CRUD de sessoes e DTO

--- API/Models/Sessao.cs
+++ API/Models/Sessao.cs
@@ -1,19 +1,27 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using API.Models;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
-namespace API.Models
+[Table("sessoes")]
+public class Sessao
 {
-    [Table("sessoes")]
-    public class Sessao
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int IdSessao { get; set; }
-        public DateTime HoraInicio { get; set; }
-        public DateTime HoraFim { get; set; }
-        public decimal Valor { get; set; }
-        public virtual Filme IdFilme { get; set; }
-        public virtual Sala IdSala { get; set; }  
-    }
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int IdSessao { get; set; }
+
+    public DateTime HoraInicio { get; set; }
+    public DateTime HoraFim { get; set; }
+    public decimal Valor { get; set; }
+
+    // Chave estrangeira explícita para Filme
+    [ForeignKey("Filme")]
+    [Column("FilmeIdFilme")]
+    public int IdFilme { get; set; }
+    public virtual Filme Filme { get; set; }
+
+    // Chave estrangeira explícita para Sala
+    [ForeignKey("Sala")]
+    [Column("SalaIdSala")]
+    public int IdSala { get; set; }
+    public virtual Sala Sala { get; set; }
 }
